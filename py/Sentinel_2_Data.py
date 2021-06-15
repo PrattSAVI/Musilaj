2bca3ca9f5eab387d267fd1123873ffc38357bb1
Pratt SAVI <58483678+PrattSAVI@users.noreply.github.com>
2021-06-15 15:47:26 -0400

    added data

--- py/Sentinel_2_Data.py
+++ py/Sentinel_2_Data.py
@@ -1,5 +1,10 @@
 #%% VIEW Boundary
 import folium 
+import sys
+import matplotlib.pyplot as plt
+import geopandas as gpd
+
+print( sys.prefix )
 
 m = folium.Map([40.5643,29.9145], zoom_start=6)
 boundsdata = r"C:\Users\csucuogl\Desktop\test\Istanbul.geojson"
@@ -12,32 +17,29 @@ from sentinelsat import SentinelAPI
 from sentinelsat import geojson_to_wkt
 from sentinelsat import read_geojson
 
-user = 'cansucuoglu' ## change this!
-password = 'exploited_123' ## change this!
+user = 'cansucuoglu'
+password = ''#sifre_123
 
 api = SentinelAPI(user, password, 'https://scihub.copernicus.eu/dhus')
-
 footprint = geojson_to_wkt(read_geojson(boundsdata))
 
 print (footprint)
 
-# %% Get Boundaries
+# %% Prepare Products
+# See overlapping geometry
+
 products = api.query(footprint,
-                     date = ('20210523', '20210608'),
+                     date = ('20210611', '20210614'),
                      platformname = 'Sentinel-2',
                      processinglevel = 'Level-2A',
-                     cloudcoverpercentage = (0, 20))
+                     cloudcoverpercentage = (0, 30))
 
-# %% See overlapping geometry
-import matplotlib.pyplot as plt
-import geopandas as gpd
 
+import contextily as ctx
 areas = api.to_geodataframe(products)
 areas['i'] = [i for i in range(len(areas))]
 
 gdf2 = gpd.read_file(boundsdata)
-
-areas = areas[ areas['i'].isin( [1,3,5,6,7,8,21] ) ]
 
 f, ax = plt.subplots(1)
 areas.to_crs(epsg=3857).plot(ax=ax,column='uuid',cmap=None,alpha=0.35)
@@ -45,47 +47,40 @@ areas.to_crs(epsg=3857).apply(lambda x: ax.annotate(s=x.i, xy=x.geometry.centroi
 gdf2.to_crs(epsg=3857).plot(ax=ax , facecolor='None' , edgecolor='red')
 ctx.add_basemap(ax)
 plt.show()
+
 # %% Choose One to Start
 import contextily as ctx
 print( len( areas ))
 
-# 1 Kapidag
-# 3 Guney Dogu Marmara
-# 5 Canakkale Korfez
-# 6 Istanbul / Izmit
-# 7 istanbul
-# 8 Kuzey Bati
-# 21 Izmit Korfez
-
-
-single = areas[ areas['i'] == 6 ]
+single = areas[ areas['i'] == 9 ]
 ax = single.to_crs(epsg=3857).plot( alpha = 0.25)
 gdf2.to_crs(epsg=3857).plot(ax=ax , facecolor='None' , edgecolor='red')
 ctx.add_basemap(ax)
 print( single['title'].tolist() )
 
-#%%
+#%% Plot one last time
+
 import contextily as ctx
 print( len( areas ))
-areas['i'] = [i for i in range(len(areas))]
 
-# 1 Kapidag
-# 3 Guney Dogu Marmara
-# 5 Canakkale Korfez
-# 6 Istanbul / Izmit
-# 7 istanbul
-# 8 Kuzey Bati
-# 21 Izmit Korfez
+remove = [1,2,3]
 
-all = [1,3,5,6,7,8,21]
+single = areas[ ~areas['i'].isin(remove) ]
+ax = single.to_crs(epsg=3857).plot( alpha = 0.25)
+gdf2.to_crs(epsg=3857).plot(ax=ax , facecolor='None' , edgecolor='red')
+single.to_crs(epsg=3857).apply(lambda x: ax.annotate(s=x.i, xy=x.geometry.centroid.coords[0], ha='center'),axis=1)
+ctx.add_basemap(ax)
 
-for a in all:
+
+#%%
+import contextily as ctx
+remove = [1,2,3,7]
+
+single = areas[ ~areas['i'].isin(remove) ]
+print( len(single) )
+for a in single['i'].unique().tolist():
     single = areas[ areas['i'] == a ]
-    #ax = single.to_crs(epsg=3857).plot( alpha = 0.25)
-    #gdf2.to_crs(epsg=3857).plot(ax=ax , facecolor='None' , edgecolor='red')
-    #ctx.add_basemap(ax)
-    #display( single[['uuid']] )
-    api.download( single['uuid'].tolist()[0])
+    api.download( single['uuid'].tolist()[0] )
 
 
 
--- py/Sentinel_2_Read_Merge.py
+++ py/Sentinel_2_Read_Merge.py
@@ -1,35 +1,33 @@
 
 
-#%%
+# This code merges all raster data in a folder.
+# Data is from Sentinel Images. 
+# Change the Env to one that can run full rasterio !!!!.
+#%% DEPENDECIES
+#Make sure the prefix is set to Remote Raster Env
 import sys
-print( sys.prefix )
-
-# %% OPEN AND MERGE DATA / IMAGE
-
 import rasterio
 import os
+import pandas as pd
+print( sys.prefix )
 
-folder_path = r'C:\Users\csucuogl\Dropbox\Musilaj_Test\210606'
+# %% Sentinel Folders
+
+folder_path = r'D:\SAVI\Musilaj_Mapping\RasterData\210613'
 folder_list = os.listdir( folder_path )
 
-print ( folder_list )
-
+[i for i in folder_list]
 
 #%% Create a file list for Bands.
 # Creates a DF to use for merging
-
-import pandas as pd
 
 bands = ['B04','B11','B8A']
 df = pd.DataFrame(columns=['folder','file','date','band'])
 
 for band in bands:
     for this_file in folder_list:
-        #print ( this_file )
-        #print( this_file.split("_")[-1].split("T")[0] )
 
         path = os.path.join( folder_path , this_file)
-        path  = os.path.join( path , this_file+".SAFE" )
         path = os.path.join( path , "GRANULE")
         path = os.path.join( path , os.listdir( path )[0] )
         path = os.path.join( path , "IMG_DATA")
@@ -47,34 +45,47 @@ for band in bands:
                 df = df.append( temp )
 
 df.index = [i for i in range(len(df))]
+df['pos'] = [r.split('_')[0] for i,r in df['file'].iteritems()]
+
+df = df[['pos','band','date','file','folder']]
+
+df = df[ ~df['pos'].str.contains("TTL") ] #TTL is projected to N36 better remove it
 
 df
+
 
 #%% MERGE AND PLOT
 
 import matplotlib.pyplot as plt
 import numpy as np
-
-df['pos'] = [r.split('_')[0] for i,r in df['file'].iteritems()]
+import rasterio
+from rasterio.merge import merge
+from rasterio.plot import show
 
 def merge_plot( df , band , filename):
-    df4 = df[ (df['band'] == band ) ]
+    df4 = df.copy()
     display( df4 )
 
     images=[]
 
     for i,r in df4.iterrows():
-        if i!=6 : #Check this
-            path = os.path.join( r['folder'] , r['file'] ) 
-            dataset = rasterio.open( path )
-            images.append( dataset )
+        path = os.path.join( r['folder'] , r['file'] ) 
+        dataset = rasterio.open( path )
+
+        images.append( dataset )
 
     #Merge
-    mosaic, out_transform =  rasterio.merge.merge(images)
+    mosaic, out_transform =  merge(images)
+
+    # test -> removing 0 value
+    mosaic = mosaic.astype( np.float32 )
+    mosaic[mosaic==0] = np.nan
+
+    print( mosaic.dtype )
 
     #Plot
     plt.figure( figsize = (12,8) )
-    rasterio.plot.show( mosaic , cmap='terrain')
+    show( mosaic , cmap='terrain')
 
     out_fp = os.path.join( folder_path , filename )
 
@@ -86,7 +97,9 @@ def merge_plot( df , band , filename):
                     "height": mosaic.shape[1],
                     "width": mosaic.shape[2],
                     "transform": out_transform,
-                    "crs": "+proj=utm +zone=35 +ellps=WGS84 +datum=WGS84 +units=m +no_defs "
+                    "crs": "+proj=utm +zone=35 +ellps=WGS84 +datum=WGS84 +units=m +no_defs ",
+                    "dtype" : mosaic.dtype,
+                    "nodata": np.nan
                     }
                     )
 
@@ -94,10 +107,16 @@ def merge_plot( df , band , filename):
     with rasterio.open(out_fp, "w", **out_meta) as dest:
         dest.write(mosaic)
 
+#Filter unique dates and bands and feed into the mergeplot
+for b in df['band'].unique().tolist():
+    temp1 = df[ df['band'] == b ]
+    for t in temp1['date'].unique().tolist():
 
-for b in bands:
-    merge_plot( df , b , '210606_{}.tiff'.format(b) )
+        temp2 = temp1[ temp1['date'] == t ]
+        filename = '{}_{}_Del.tiff'.format(t,b)
 
+        print( filename )
+        merge_plot( temp2 , b , filename )
 
 
 # %%
